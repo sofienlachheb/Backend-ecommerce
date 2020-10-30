25a104638bff024e4cddf7a5ccaee1f0d92e15e1
eakpoli <eakpoli@gmail.com>
2020-10-30 17:53:19 +0100

    WEB SERVICE v1.0.4

--- config/config.php
+++ config/config.php
@@ -10,5 +10,14 @@
     define("HOST", "localhost");
     define("DB_NAME", "ecommerce");
 
+    $METHODES = [
+      "get"=>["description"=>"Lire les données","prefixe"=>"get" ],
+      "post"=>["description"=>"Créer une donnée","prefixe"=>"create" ],
+      "put"=>["description"=>"Mettre à jour une donnée","prefixe"=>"update" ],
+      "delete"=>["description"=>"Supprimer une donnée", "prefixe"=>"delete"],
+       
+    ];
+
+    $_ROUTES = ["products", "category", "orders","users"];
 
 ?>
--- index.php
+++ index.php
@@ -1,72 +1,93 @@
- 
-<?php 
-  define("BASE_URL", dirname($_SERVER['SCRIPT_NAME']));
- 
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+require 'config/config.php';
+define("BASE_URL", dirname($_SERVER['SCRIPT_NAME']));
 
-    <!-- Bootstrap CSS -->
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
  <style>
-     body{
-         background-color: #68cdce;
+   
+     a{
+       color: black;
+     }
+     p{
+      background-color: #fff; 
+      font-size: 35px;
+      border: 1px solid rgba(0,0,0,.1);
+      box-shadow:
+        0 10px 20px rgba(0,0,0,.22),
+        0 14px 56px rgba(0,0,0,.25);
+      
+     }
+     p span:nth-child(1){
+       display: inline-block;
+        font-size: 44px;
+        font-weight: 700;
+        min-width: 200px;
+        padding: 6px 15px;
+        text-align: center;
+        color: #fff;
+     }
+     p span:nth-child(2){
+      font-size: 35px;
+      font-weight: 700;
+     }
+     .get{
+      background-color: #3caab5;
+      text-transform: uppercase;
+     }
+     .post{
+      background-color: #78bc61;
+      text-transform: uppercase;
+     }
+     .delete{
+      background-color: #f93e3e;
+      text-transform: uppercase;
+     }
+     .put{
+      background-color: #fca130;
+      text-transform: uppercase;
      }
      nav.navbar{
-         background-color: #1c3335 !important;
+         background-color: #288690 !important;
      }
 
  </style>
     <title>API JSORE !</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
         <a class="navbar-brand" href="#">JSTORE API</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        
     </nav>
-
     <div class="container">
-    <ul class="list-group  m-5">
+      <h1 class="text-center display-4">Documentation API JSTORE</h1>
+  
     <?php
-        $nom_dossier = 'api/';
-        $dossier = opendir($nom_dossier);
-
-
-        while($fichier = readdir($dossier))
-        {
-            if($fichier != '.' && $fichier != '..')
-            {
-            echo '<li class="list-group-item m-2">
-          
-            <var>Route : <a href="'.BASE_URL.'/api/'.$fichier.'" target="_blank">'.BASE_URL.'/api/'.$fichier.'</a></var>
-            <p><strong>Description : </strong></p>
-            <p><strong>Paramètres : </strong></p>
-            <p><strong>Format de données retournées : </strong>Json</p>
-            </li>';
+        
+        // Ouverture du dossier API
+        foreach ($_ROUTES as $key => $entity) {
+            $response = "<div id='$entity' class='display-4'><h4>".ucwords($entity)."</h4>";
+            foreach ($METHODES as $methode => $description) {
+              $response .= "<p><span class='$methode'> $methode </span> 
+              <span class='url'>
+              <a href='".BASE_URL."/api/$entity'target='_blank'> /api/$entity</a>
+              </span> 
+              ".$description['description']." : $entity</p>";
             }
+            echo $response.'</div>';
         }
-        closedir($dossier);
+
     ?>
 
-        
-    </ul>
+</body>
 
-    </div>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
 </html>
-
