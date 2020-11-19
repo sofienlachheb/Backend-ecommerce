045fca4ab1cacae3bfb49d951a7e7026c4984603
Espero AKPOLI <contact@espero-soft.com>
2020-11-20 00:11:24 +0100

    update files

--- api/commun_services.php
+++ api/commun_services.php
@@ -1,6 +1,8 @@
 <?php 
 date_default_timezone_set("Europe/Paris");
 header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
 define("API", dirname(__FILE__));
 define("ROOT", dirname(API));
